43854bacf1da9e3e28e4abd97eec77e600282c79
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-17 19:05:56 +0200

    deleting lines

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,15 +5,6 @@ using namespace std;
 
 int main () {
 	printf("hello, world\n");
-	cout << boolalpha;
-	cout << true << " " << false << endl;
-	printf("This is integer literal %d\n", 10);
-  	printf("This is integer literal in hex format 0xA =  %d\n", 0xA);
-  	printf("This is integer literal represent in octal format %o\n", 10);
-  	printf("This is float literal %f\n", 1.25);
-  	printf("This is float literal %f\n", 1.25e1);
-  	printf("This is char literal %c\n", 'c');
-  	printf("This is string literal %s\n", "string");
-printf("This is string literal %s\n", "new_line added");
+
 return 0;
 }
