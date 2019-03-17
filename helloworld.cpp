223f3c40bc4078c0b05c2dca23ff96ecf30c92ba
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-17 18:44:12 +0200

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "iostream"
+
+using namespace std;
+
+int main () {
+	printf("hello, world\n");
+	cout << boolalpha;
+	cout << true << " " << false << endl;
+return 0;
+}
